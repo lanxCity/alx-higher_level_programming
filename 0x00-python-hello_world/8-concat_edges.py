30d6c9e5c396a6f90a39b072790f9002d976f89b
Abdulramon Sodiq <Sodiqramon0@gmail.com>
2024-03-11 06:58:23 -0700

    Add project 8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = 'Python is an interpreted, interactive, object-oriented programming' \
-        'language that combines remarkable power with very clear syntax'
-str = "object-oriented programming with Python"
+        ' language that combines remarkable power with very clear syntax'
+str = str[str.index("object"):66] + " with " + str[:6]
 print(str)
