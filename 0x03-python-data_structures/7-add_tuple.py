22b7e7959d81b3d0a7697fb38b17d7d59bc0f040
Abdulramon Sodiq <sodiqramon0gmail.com>
2024-03-17 09:45:04 +0000

    Add project 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,9 +3,17 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     max_tuple = max(len(tuple_a), len(tuple_b))
 
+    if max_tuple == 0:
+        return (0, 0)
+
     if len(tuple_a) == 0:
+        if tuple_b == 1:
+            return (tuple_b[0], 0)
         return tuple_b
+
     if len(tuple_b) == 0:
+        if tuple_a == 1:
+            return (tuple_a[0], 0)
         return tuple_a
 
     if max_tuple >= 2:
