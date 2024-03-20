7de91f9fef3822a92ed5805fe27177931f0ea7f5
Abdulramon Sodiq <sodiqramon0gmail.com>
2024-03-20 17:25:48 +0000

    Correct project 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,19 +2,7 @@
 def square_matrix_simple(matrix=[]):
     matrix_cpy = matrix[:]
     counter = 0
-
-    for row in matrix_cpy:
+    for row in matrix:
         matrix_cpy[counter] = list(map((lambda x: x * x), row))
         counter += 1
-
     return matrix_cpy
-
-    '''
-    for i in range(len(matrix_cpy)):
-        new_matrix[len(new_matrix):] = [[]]
-
-        for j in range(len(matrix_cpy[i])):
-            sqr_el = matrix_cpy[i][j] * matrix_cpy[i][j]
-            new_matrix[i][len(new_matrix[i]):] = [sqr_el]
-    return new_matrix
-    '''
