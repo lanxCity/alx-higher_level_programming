dd3147260c8a7f54b665d5a1a4ca54cccdb4645e
Abdulramon Sodiq <sodiqramon0gmail.com>
2024-03-20 16:47:44 +0000

    Add project 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     new_matrix = []
-
     for i in range(len(matrix)):
         new_matrix.append([])
-
         for j in range(len(matrix[i])):
-
             sqr_el = matrix[i][j] * matrix[i][j]
-
             new_matrix[i].append(sqr_el)
-
     return new_matrix
