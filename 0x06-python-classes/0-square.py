68f5f1ff9a6aab2c19eb447a90700e41823a2454
Abdulramon Sodiq <Sodiqramon0@gmail.com>
2024-05-20 02:22:51 -0700

    Add project 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,9 @@
+#!/usr/bin/python3
 class Square:
-    """
-    this is an empty class
+    """ this is an empty class
+    Avoiding the Esc key
+    >>If you have an American English keyboard, pressing Ctrl-[
+    (control plus left square bracket) is equivalent to pressing Esc
+    This provides an easy way to exit from insert mode.
     """
     pass
