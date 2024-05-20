fca9841ae28651ee364a527f67d31101cee35642
Abdulramon Sodiq <sodiqramon0gmail.com>
2024-05-20 15:41:22 +0000

    Add project 6a

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -59,7 +59,6 @@ class Square:
             return True
 
         if not isinstance(value, tuple) or\
-                len(value) != 2 or\
                 not is_int(value):
 
             raise TypeError("position must be a tuple of 2 positive integers")
