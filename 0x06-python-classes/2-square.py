e6b05d3ecec6dc8931d73bafa2c608951c07e497
Abdulramon Sodiq <sodiqramon0gmail.com>
2024-05-20 12:45:52 +0000

    Add project 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Intro to python object oriented programming """
+
+
+class Square:
+    """
+    A class representing a square shape.
+
+    This class provides a blueprint for creating square objects.
+    Squares are geometric shapes with four equal sides
+    and four right angles.
+
+    Attributes:
+        _size (int): Size of a square
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new Square object.
+        Args:
+            size (int): Size of a square
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
