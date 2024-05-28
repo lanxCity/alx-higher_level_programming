c941f0a018a88e700804d767198326c76f868bd0
Abdulramon Sodiq <Sodiqramon0@gmail.com>
2024-05-28 03:19:38 -0700

    Add project 8

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- script that displays the number of records with id = 89
+-- in the table first_table of the database hbtn_0c_0 in your MySQL server
+-- SELECT * FROM first_table where id=89   -> To list a the data with same id
+SELECT COUNT(*) FROM first_table where id=89
