9148fffbf690dcb44fe6d2b04165826423437163
Abdulramon Sodiq <sodiqramon0gmail.com>
2024-06-20 12:17:16 +0000

    Add project 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,40 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  /**
+   * Prints the rectangle using the character X
+   */
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let line = '';
+      for (let j = 0; j < this.width; j++) {
+        line += 'X';
+      }
+      console.log(line);
+    }
+  }
+
+  /**
+   * Swaps the values of width and height of the rectangle
+   */
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  /**
+   * Multiples the width and the height of the rectangle by 2
+   */
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
