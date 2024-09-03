48ca1198a6ee0110feb873dabdba311526505529
Abdulramon Sodiq <sodiqramon0@gmail.com>
2024-09-03 23:50:46 +0100

    project 1 updated-1

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,4 +5,37 @@
 
 
 def matrix_divided(matrix, div):
-    if not matrix or 
+    # -> Type error
+    if (
+            # Empty matrix
+            not matrix or
+            # check rows data types
+            (not all(isinstance(row, list) for row in matrix)) or
+            # check empty rows
+            (not all([True if row else False for row in matrix][:])) or
+            # Check cols data type
+            (not all([True if type(col) in [int, float] else False
+                      for row in matrix for col in row][:]))
+    ):
+        raise TypeError('matrix must be a matrix '
+                        '(list of lists) of integers/floats')
+
+    # -> Rows lengths
+    rows_lengths = [len(row) for row in matrix]
+    min_len, max_len = (min(rows_lengths), max(rows_lengths))
+    if min_len != max_len:
+        raise TypeError('Each row of the matrix must have the same size')
+
+    # -> div data type
+    if type(div) not in [int, float]:
+        raise TypeError('div must be a number')
+
+    # -> non-zero div
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
+
+    # Answer
+    ans = list(map(lambda row:
+                   list(map(lambda col: round(col / div, 2), row)),
+                   matrix))
+    return ans
