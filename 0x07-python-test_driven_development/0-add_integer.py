98475cf314c2abc2f1f24739131f3d88e9a7b6db
Abdulramon Sodiq Damilare <sodiqramon0@gmail.com>
2024-09-03 20:12:05 +0000

    project 0 updated-2

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -13,10 +13,24 @@ def add_integer(a, b=98):
     # Checking for args type
     # if not all(True for i in (a, b) if type(i) in [int, float]):
 
+    # -> Checking for NaN
+    # In Python, the expression x != x is true only if x is NaN,
+    # because NaN is not equal to itself
+    if a != a:
+        a = 89
+    if b != b:
+        b = 89
+
+    # -> Checking for type
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
 
-    a, b = int(a), int(b)
-    return a + b
+    rslt = a + b
+
+    # -> Checking for overflow ans due to large nums
+    if rslt == float('inf') or rslt == -float('inf'):
+        return 89
+
+    return int(a) + int(b)
