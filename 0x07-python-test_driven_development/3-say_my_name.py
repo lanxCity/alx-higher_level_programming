7f9962efe65d00d8944f00333a1cadaee5e22770
Abdulramon Sodiq <sodiqramon0@gmail.com>
2024-09-15 08:02:01 +0100

    Add project 2

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -5,4 +5,13 @@
 
 
 def say_my_name(first_name, last_name=""):
-    pass
+    if not isinstance(first_name, str):
+        raise TypeError('first_name must be a string')
+    if not isinstance(last_name, str):
+        raise TypeError('last_name must be a string')
+
+    # if first_name.isalpha() and last_name.isalpha():
+    return f'{first_name} {last_name}'
+
+
+
