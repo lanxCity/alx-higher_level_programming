4bb7b56967f2eba1e573f2e9a3acf6d780e8c023
Abdulramon Sodiq <sodiqramon0@gmail.com>
2024-09-16 10:50:14 +0100

    Add project 3

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    A function that prints a square with the character #
+"""
+
+
+def print_square(size):
+    """
+    Args:
+        size int:  the size length of the square
+
+    Returns:
+        None
+    """
+    if ((isinstance(size, float) and size < 0) or
+            not isinstance(size, int)):
+        raise TypeError('size must be an integer')
+
+    if size < 0:
+        raise ValueError('size must be >= 0')
+
+    for row in range(size):
+        print('#' * size)
+
+    return
