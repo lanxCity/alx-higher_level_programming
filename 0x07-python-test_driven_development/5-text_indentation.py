8d9861de38605c4c6b914cde58f11387dffb2336
Abdulramon Sodiq Damilare <sodiqramon0@gmail.com>
2024-09-16 15:10:40 +0000

    Add project 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -10,17 +10,25 @@ def text_indentation(text):
         text str: a string of characters
 
     Returns:
-
+        None
     """
     if type(text) is not str:
         TypeError('text must be a string')
 
     new_line = True
-    for i in text:
-        print(i if new_line and i != ' ' else '',
-              end='')
-        new_line = False
+    line = ''
 
-        if i in '.?:':
-            print('\n')
-            new_line = True
+    for i in range(len(text)):
+        char = text[i]
+
+        if char == ' ' and not line:
+            char = ''
+
+        line += char
+
+        if char in '.?:':
+            line += '\n'
+            print(line)
+
+            line = ''
+    return
